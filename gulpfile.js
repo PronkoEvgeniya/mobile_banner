c15eb05f3f46098df0610a9790b0ce4876ee085c
PronkoEvgeniya <Shreidereva@gmail.com>
2023-03-15 15:27:45 +0300

    init: create app framework and basic setting gulp

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const autoPrefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+
+function styles() {
+  return src('app/assets/style/style.scss')
+    .pipe(autoPrefixer({ overrideBrowsersList: ['last 10 version'] }))
+    .pipe(concat('style.min.css'))
+    .pipe(dest('app/assets/css'))
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(browserSync.stream());
+}
+
+function scripts() {
+  return src('app/js/main.js')
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream());
+}
+
+function watching() {
+  watch(['app/assets/style/style.scss'], styles);
+  watch(['app/js/main.js'], scripts);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
+}
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'app/',
+    },
+  });
+}
+
+function cleanDist() {
+  return src('dist').pipe(clean());
+}
+
+function building() {
+  return src(['app/assets/css/style.min.css', 'app/js/main.min.js', 'app/**/*.html'], {
+    base: 'app',
+  }).pipe(dest('dist'));
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.build = series(cleanDist, building);
+
+exports.default = parallel(styles, scripts, watching, browsersync);
