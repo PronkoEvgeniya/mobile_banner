18ee8a85b15eeafb143e0f38dc7ad16b511c6bc9
PronkoEvgeniya <Shreidereva@gmail.com>
2023-03-17 12:29:00 +0300

    feat: add functional translation

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,18 @@
-const doc = [1, 2, 3];
+const transCollection = document.querySelectorAll('[data-i18n]');
+const langs = ['en', 'es', 'fr', 'ja', 'nl', 'ru', 'zh'];
+const userLang = (navigator.language || navigator.userLanguage).slice(0, 2);
+const param = window.location.search.slice(-2);
+let selectLang = 'en';
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (langs.includes(param)) {
+    selectLang = param;
+  } else if (langs.includes(userLang)) {
+    selectLang = userLang;
+  }
+  fetch(`../assets/localizations/${selectLang}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      transCollection.forEach((elem) => (elem.textContent = data[elem.dataset.i18n]));
+    });
+});
